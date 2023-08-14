ff1e7d9fdb8f7c8be3c9950f9e32cd8236336f98
Ybarkati <yassinebarkati47@gmail.com>
2023-08-14 17:58:42 -0600

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require("express")
 const app= express()
 const PORT=3000
 const pokemon=require("./models/pokemon")
+const jsxEngine = require('jsx-view-engine')
 
+// Load the method override function
+
+// Configure the view engine and look for files ending in jsx
+app.set('view engine', 'jsx')
+
+// Create the engine and accept files ending in jsx
+app.engine('jsx', jsxEngine())
 app.get("/",(req,res)=>{
     res.send("Welcome to the Pokemon App!")
 })
 app.get("/pokemon",(req,res)=>{
-   res.send(pokemon)
+   res.render("Index")
 })
 app.listen(PORT,()=>{
     console.log("listen in : "+PORT)
---
+++ views/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Index(){
+    const myStyle = {
+        color: '#ffffff',
+        backgroundColor: '#000000',
+        };
+    return(
+        <h1 style={myStyle}>See All The Pokemon!</h1>
+    )
+}
